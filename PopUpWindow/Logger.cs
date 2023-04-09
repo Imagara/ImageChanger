1c6d9eab98d8ce626c5807c55898ab2a4bd882b8
Imagara!? <milick33@gmail.com>
2023-04-10 02:31:22 +0700

    Щptimization and improvement of the logger

--- PopUpWindow/Logger.cs
+++ PopUpWindow/Logger.cs
@@ -6,43 +6,58 @@ namespace PopUpWindow;
 
 public class Logger
 {
-    private static readonly string DirPath = Environment.CurrentDirectory + MainSettings.Slash + "logs";
+    // The path where log files will be stored.
+    private static readonly string DirPath = $"{Environment.CurrentDirectory}{Path.DirectorySeparatorChar}logs";
+
+    // The name of the log file. This will be the current date in the format MM/dd/yyyy.
     private static readonly string LogFileName = $"{DateTime.Now.ToShortDateString()}.log";
-    private readonly string _logFilePath = DirPath + MainSettings.Slash + LogFileName;
+
+    // The full path to the log file.
+    private readonly string _logFilePath = $"{DirPath}{Path.DirectorySeparatorChar}{LogFileName}";
+
+    // Constructor. Creates the log directory if it doesn't exist and creates the log file if it doesn't exist.
     public Logger()
     {
-        FileInfo logFile = new FileInfo(_logFilePath);
         DirectoryInfo dir = new DirectoryInfo(DirPath);
-        
         if (!dir.Exists)
+        {
             Directory.CreateDirectory(DirPath);
-        
+        }
+
+        FileInfo logFile = new FileInfo(_logFilePath);
         if (!logFile.Exists)
+        {
             CreateLogFile();
+        }
     }
 
+    // Creates a log file and deletes all old log files if there are more than 3.
     private void CreateLogFile()
     {
         try
         {
-           
             FileInfo logFile = new FileInfo(_logFilePath);
             if (!logFile.Exists)
-                logFile.Create();
-
-           DirectoryInfo dir = new DirectoryInfo(DirPath);
-           if (dir.GetFiles().Count(item => item.Extension == ".log") > 3)
             {
-                FileInfo logFileDelete = dir.GetFiles().OrderBy(item => item.CreationTime).First();
-                logFileDelete.Delete();
+                logFile.Create().Close();
+            }
+
+            DirectoryInfo dir = new DirectoryInfo(DirPath);
+            FileInfo[] logFiles = dir.GetFiles("*.log");
+
+            if (logFiles.Length > 3)
+            {
+                FileInfo logFileDelete = logFiles.OrderBy(item => item.CreationTime).FirstOrDefault();
+                logFileDelete?.Delete();
             }
         }
-        catch (Exception e)
+        catch (IOException ex)
         {
-            // ignored
+            Console.WriteLine($"Error: {ex.Message}");
         }
     }
 
+    // Writes the specified error message to the log file.
     public void CreateLog(string error)
     {
         FileInfo logFile = new FileInfo(_logFilePath);
@@ -51,13 +66,14 @@ public class Logger
         {
             try
             {
-                StreamWriter sw = new StreamWriter(_logFilePath, true);
-                sw.WriteLine(DateTime.Now + "|" + error);
-                sw.Close();
+                using (StreamWriter sw = File.AppendText(_logFilePath))
+                {
+                    sw.WriteLine($"{DateTime.Now} | {error}");
+                }
             }
-            catch
+            catch (IOException e)
             {
-                // ignored
+                Console.WriteLine($"An error occurred while writing to the log file: {e.Message}");
             }
         }
     }
